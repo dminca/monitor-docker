303f3c19ea5b2325634d4253d2a9e74241ed7917
Minca Daniel Andrei <dminca@marketstheworld.com>
2016-08-17 16:41:46 +0300

    Add argument to remove Docker Containers also
    After stopping the Containers, also remove them with rogue or useless
    ones (this also deletes the created Network)
    
    Resolves:
    Signed-off-by: Minca Daniel Andrei <dminca@marketstheworld.com>

--- ass.py
+++ ass.py
@@ -5,12 +5,33 @@ import sys
 
 # remove the contents of data/*
 data_dir = "data/*"
-cmdping = "sudo rm -rfv " + data_dir
-p = subprocess.Popen(cmdping, shell=True, stderr=subprocess.PIPE)
-while True:
-    out = p.stderr.read(1)
-    if out == '' and p.poll() != None:
-        break
-    if out != '':
-        sys.stdout.write(out)
-        sys.stdout.flush()
+
+def rmv():
+    cmdping = "sudo rm -rfv " + data_dir
+    p = subprocess.Popen(cmdping, shell=True, stderr=subprocess.PIPE)
+    while True:
+        out = p.stderr.read(1)
+        if out == '' and p.poll() != None:
+            break
+        if out != '':
+            sys.stdout.write(out)
+            sys.stdout.flush()
+
+def remove_containers():
+    rmv_containers = "docker compose down -v --remove-orphans"
+    p = subprocess.Popen(rmv_containers, shell=True, stderr=subprocess.PIPE)
+    while True:
+        out = p.stderr.read(1)
+        if out == '' and p.poll() != None:
+            break
+        if out != '':
+            sys.stdout.write(out)
+            sys.stdout.flush()
+
+if sys.argv[1] == 'rf':
+    rmv()
+elif sys.argv[1] == 'down':
+    #print(sys.argv)
+    remove_containers()
+else:
+    exit()
